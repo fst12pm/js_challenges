7b48eed000ddae19747a39265d3037a28443568e
= <= AhmedITfixer@gmail.com>
2023-10-05 20:10:59 +0100

    I added extra stuff

--- practice.js
+++ practice.js
@@ -1,15 +1,18 @@
+/* I practiced my for loop*/
 for(let i =0;i < 10;i++){
     console.log("Ahmed")
     i++
 }
-
+/*I practiced while loop */
 let count = 0
 while(count < 10){
     count++
     console.log("Mohamed")
 }
 
+/*I practiced do while loop */
 let c = 0;
+
 do{
     c++
     console.log("Noor")
