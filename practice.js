6c11d47cb02dae5828706595990dcefae0709c53
= <= AhmedITfixer@gmail.com>
2023-10-06 21:08:41 +0100

    second commit

--- practice.js
+++ practice.js
@@ -3,14 +3,14 @@ for(let i =0;i < 10;i++){
     console.log("Ahmed")
     i++
 }
-/*I practiced my while loop */
+/*I practiced while loop */
 let count = 0
 while(count < 10){
     count++
     console.log("Mohamed")
 }
 
-/*I practiced my do while loop */
+/*I practiced do while loop */
 let c = 0;
 
 do{
