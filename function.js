18ba32d61680532afe32885d45df7f9e313cb004
= <= AhmedITfixer@gmail.com>
2023-10-07 13:40:28 +0100

    I added an arrow function

--- function.js
+++ function.js
@@ -1,5 +1,49 @@
-function multiply(num1,num2){
-    return num1*num2
-}
+// write a simple JavaScript function called multiply that takes two numbers as params and returns the result to the caller
+// function multiply(num1, num2) {
+//     return num1 * num2
+// }
+
+// let result = multiply(5,6)
+// console.log(result)
+
+
+// convert the function above to a variable
+
+// let multiply =  function (num1, num2) {
+//     return num1 * num2
+// }
+
+// let result = multiply(5,6)
+// console.log(result)
+
+
+// convert the multiply function above to an arrow function
+
+//let multiply =  (num1, num2) =>  num1 * num2
+
+
+//let result = multiply(5,6)
+// console.log(result// write a simple JavaScript function called multiply that takes two numbers as params and returns the result to the caller
+// function multiply(num1, num2) {
+//     return num1 * num2
+// }
+
+// let result = multiply(5,6)
+// console.log(result)
+
+
+// convert the function above to a variable
+
+// let multiply =  function (num1, num2) {
+//     return num1 * num2
+// }
+
+// let result = multiply(5,6)
+// console.log(result)
+
+
+// convert the multiply function above to an arrow function
+
+let multiply = (num1,num2) => num1*num2
 let result = multiply(5,6)
 console.log(result)
